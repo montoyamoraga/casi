4ea1b244c6fc36a80b9023fce485ee36897fbba5
montoyamoraga <montoyamoraga@gmail.com>
2021-02-11 23:25:19 -0500

    add sketch and references

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # casi
+
+## References
+
+lmccart
+https://gist.github.com/stalgiag/67a763878ebe2348e7fa9ef03ec5e0ec
+
+stalgiag
+https://gist.github.com/stalgiag/67a763878ebe2348e7fa9ef03ec5e0ec
--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,42 @@
+let canvas;
+
+let capture;
+
+let tracker;
+
+let debugMode = false;
+
+// setup() runs once, at the beginning
 function setup() {
-  createCanvas(400, 400);
-  background(0, 0, 255);
+
+  capture = createCapture(VIDEO);
+  capture.size(400, 300);
+  capture.position(0, 0);
+
+  canvas = createCanvas(400, 300);
+  canvas.style('z-index', 9999);
+  canvas.position(0, 0);
+
+  tracker = new clm.tracker();
+  tracker.init();
+  tracker.start(capture.elt);
+
+  fill(255);
+  noStroke();
 }
 
+// draw(), runs in a loop, after setup()
 function draw() {
-  
+
+  clear();
+
+  let positions = tracker.getCurrentPosition();
+
+  if (positions.length > 0) {
+    // eyes info between indices 23 and 32
+    for (let i = 23; i <= 32 ; i++) {
+      ellipse(positions[i][0], positions[i][1], 8, 8);
+    }  
+  }
+
 }
